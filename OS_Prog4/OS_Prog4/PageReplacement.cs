e96d60ee641918ef84f54abae6c74cdaaabf7060
Josh Schultz <schultz.josh91@yahoo.com>
2014-05-04 22:56:41 -0600

    Added Optimal and LFU to PageReplacement.cs

--- OS_Prog4/OS_Prog4/PageReplacement.cs
+++ OS_Prog4/OS_Prog4/PageReplacement.cs
@@ -434,6 +434,427 @@ namespace OS_Prog4
         }
 
 
+        //*******************************************************************//
+        //Author: Josh Schultz
+        //
+        //Date: May 2, 2014
+        //
+        //Description:  This method uses the public property, ReferenceString
+        //              to simulate the contents of three frames at each iteration.
+        //              The simulation is based on an optimal page replacement.
+        //
+        //Parameters:   (nothing)
+        //
+        //Returns:  frames - conents of frames through each iteration 
+        //*******************************************************************//
+        public ObservableCollection<ObservableCollection<string>> Optimal()
+        {
+            //Skip the ordering if there are no references
+            if (ReferenceString.Count <= 0)
+            {
+                return new ObservableCollection<ObservableCollection<string>>();
+            }
+
+            //Array to store each frame iteration
+            ObservableCollection<ObservableCollection<string>> frames = new ObservableCollection<ObservableCollection<string>>();
+
+            //The current contents of the frame
+            List<int> frameContents = new List<int>(3);
+
+            //Initialize the contents to an invalid number
+            for (int i = 0; i < 3; i++)
+            {
+                frameContents.Add(-1);
+            }
+
+            //Initialize the frames with 3 frames
+            for (int frameCount = 4; frameCount > 0; frameCount--)
+            {
+                ObservableCollection<string> row = new ObservableCollection<string>();
+                for (int i = 0; i < ReferenceString.Count; i++)
+                    row.Add("");
+                frames.Add(row);
+            }
+
+            //Total number of faults
+            int faults = 1;
+
+            //Insert the first element
+            frames[0][0] = ReferenceString[0].ToString();
+            frames[3][0] = "F";
+            frameContents[0] = ReferenceString[0];
+
+            for (int i = 1; i < Length; i++)
+            {
+                //If the current page exists in the existing frame table
+                if (ReferenceString[i] == frameContents[0] ||
+                    ReferenceString[i] == frameContents[1] ||
+                    ReferenceString[i] == frameContents[2])
+                {
+                    //Copy from the previous frame
+                    frames[0][i] = frames[0][i - 1];
+                    frames[1][i] = frames[1][i - 1];
+                    frames[2][i] = frames[2][i - 1];
+                }
+                else
+                {
+                    //The item wasn't found in the frames
+
+                    //Since it wasn't found, generate a fault
+                    frames[3][i] = "F";
+                    faults++;
+
+                    //Determine if there is an open frame
+                    if (frameContents[1] == -1)
+                    {
+                        //Insert into the open frame
+                        frames[2][i] = "";
+                        frames[1][i] = frames[0][i - 1];
+                        frames[0][i] = ReferenceString[i].ToString();  //newest element
+
+                        //Push down the queue
+                        frameContents[1] = frameContents[0];
+                        frameContents[0] = ReferenceString[i];
+                        continue;
+                    }
+                    else if (frameContents[2] == -1)
+                    {
+                        //Insert into the open frame
+                        frames[2][i] = frames[1][i - 1];
+                        frames[1][i] = frames[0][i - 1];
+                        frames[0][i] = ReferenceString[i].ToString();
+
+                        //Push down the queue
+                        frameContents[2] = frameContents[1];
+                        frameContents[1] = frameContents[0];
+                        frameContents[0] = ReferenceString[i];
+                        continue;
+                    }
+
+                    //Determine the next values referenced in the string
+                    bool[] usedSoon = new bool[3];
+                    usedSoon[0] = false;
+                    usedSoon[1] = false;
+                    usedSoon[2] = false;
+
+                    //Number of items in frame that will be referenced in the future
+                    int numReferencedSoon = 0;
+
+                    //The element wasn't in the frames and there wasn't an open frame
+                    for (int j = i + 1; j < ReferenceString.Count; j++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            //The a later item in the Reference string is currently in the frame contents
+                            if (frameContents[k] == ReferenceString[j])
+                            {
+                                usedSoon[k] = true;
+                                numReferencedSoon++;
+                            }
+
+                            //If two will be referenced soon, kick out the third
+                            if (numReferencedSoon == 2)
+                            {
+                                //Update the current frame contents
+                                if (!usedSoon[0])
+                                {
+                                    frameContents[0] = ReferenceString[i];
+                                }
+                                else if (!usedSoon[1])
+                                {
+                                    //Push the newest item at the top
+                                    frameContents[1] = frameContents[0];
+                                    frameContents[0] = ReferenceString[i];
+                                }
+                                else if (!usedSoon[2])
+                                {
+                                    //Push the newest item at the top of the frames
+                                    frameContents[2] = frameContents[1];
+                                    frameContents[1] = frameContents[0];
+                                    frameContents[0] = ReferenceString[i];
+                                }
+
+                                //force the break out of both for loops
+                                j = ReferenceString.Count;
+                                break;
+                            }
+                        } //k < 3 loop
+                    } //j < Reference.count loop
+
+                    if (numReferencedSoon < 2)
+                    {
+                        frameContents[2] = frameContents[1];
+                        frameContents[1] = frameContents[0];
+                        frameContents[0] = ReferenceString[i];
+                    }
+
+                    //Update the frame table
+                    frames[0][i] = frameContents[0].ToString();
+                    frames[1][i] = frameContents[1].ToString();
+                    frames[2][i] = frameContents[2].ToString();
+                }
+            }
+
+            //return faults;  //Number of faults
+            return frames;  //4 rows, top 3 contains numbers, last row contains F's
+        }
+
+
+
+        public ObservableCollection<ObservableCollection<string>> LeastFrequentlyUsed()
+        {
+            //Skip the ordering if there are no references
+            if (ReferenceString.Count <= 0)
+            {
+                return new ObservableCollection<ObservableCollection<string>>();
+            }
+
+            //Array to store each frame iteration
+            ObservableCollection<ObservableCollection<string>> frames = new ObservableCollection<ObservableCollection<string>>();
+
+            //List to determine how many times a page was referenced
+            List<int> referenceCounts = new List<int>(MaxPageValue);
+
+            //Default the pages to zero references
+            for (int i = 0; i < MaxPageValue + 1; i++)
+                referenceCounts.Add(0);
+
+            //The current contents of the frame
+            List<int> frameContents = new List<int>(3);
+
+            //Initialize the contents to an invalid number
+            for (int i = 0; i < 3; i++)
+            {
+                frameContents.Add(-1);
+            }
+
+            //Initialize the frames with 3 frames
+            for (int frameCount = 4; frameCount > 0; frameCount--)
+            {
+                ObservableCollection<string> row = new ObservableCollection<string>();
+                for (int i = 0; i < ReferenceString.Count; i++)
+                    row.Add("");
+                frames.Add(row);
+            }
+
+            //Total number of faults
+            int faults = 1;
+
+            //Insert the first element
+            frames[0][0] = ReferenceString[0].ToString() + "(1)";
+            frames[3][0] = "F";
+            frameContents[0] = ReferenceString[0];
+            referenceCounts[ReferenceString[0]]++;
+
+            for (int i = 1; i < Length; i++)
+            {
+                //Update the frequency count
+                referenceCounts[ReferenceString[i]]++;
+
+                //If the current page exists in the existing frame table
+                if (ReferenceString[i] == frameContents[0])
+                {
+                    if (frameContents[1] != -1 && frameContents[2] != -1)
+                    {
+                        //If the frames are full, reorder them
+                        //If they weren't full, -1 indexes would cause runtime errors
+                        Reorder(frameContents, referenceCounts, ReferenceString[i]);
+                        frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                        frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                        frames[2][i] = frameContents[2] + "(" + referenceCounts[frameContents[2]].ToString() + ")";
+                    }
+                    else if (frameContents[1] != -1)
+                    {
+                        //There are two options if one of the frames are -1
+                        //The last two frames are -1 or only the last frame is -1
+                        //This elseif determines that only the last frame is -1
+                        if (referenceCounts[frameContents[0]] < referenceCounts[frameContents[1]])
+                        {
+                            //The ordering is flipped, so fix it
+                            int temp = frameContents[0];
+                            frameContents[0] = frameContents[1];
+                            frameContents[1] = temp;
+                        }
+
+                        //Update the frames
+                        frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                        frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                        frames[2][i] = "";
+                    }
+                    else
+                    {
+                        //The last 2 frames are -1
+                        //This means only 1 actual value is in the frames
+
+                        //Update the frames
+                        frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                        frames[1][i] = "";
+                        frames[2][i] = "";
+                    }
+                    continue;
+                }
+                else if (ReferenceString[i] == frameContents[1])
+                {
+
+                    if (frameContents[2] != -1)
+                    {
+                        Reorder(frameContents, referenceCounts, ReferenceString[i]);
+                        frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                        frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                        frames[2][i] = frameContents[2] + "(" + referenceCounts[frameContents[2]].ToString() + ")";
+                    }
+                    else
+                    {
+                        //The last frame is -1, just compare the first two frames
+
+                        //Determine if the first two need flipped
+                        if (referenceCounts[frameContents[0]] < referenceCounts[frameContents[1]])
+                        {
+                            //The ordering is flipped, so fix it
+                            int temp = frameContents[0];
+                            frameContents[0] = frameContents[1];
+                            frameContents[1] = temp;
+                        }
+
+                        //Update the frames
+                        frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                        frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                        frames[2][i] = "";
+                    }
+                    continue;
+                }
+                else if (ReferenceString[i] == frameContents[2])
+                {
+                    Reorder(frameContents, referenceCounts, ReferenceString[i]);
+                    frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                    frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                    frames[2][i] = frameContents[2] + "(" + referenceCounts[frameContents[2]].ToString() + ")";
+                    continue;
+                }
+                else
+                {
+                    //The item wasn't found in the frames
+
+                    //Since it wasn't found, generate a fault
+                    frames[3][i] = "F";
+                    faults++;
+
+                    //Determine if there is an open frame
+                    if (frameContents[1] == -1)
+                    {
+                        //Insert into the open frame
+                        frames[2][i] = "";
+                        frames[1][i] = ReferenceString[i].ToString() + "(1)";
+                        frames[0][i] = frames[0][i - 1];
+
+                        frameContents[1] = ReferenceString[i];
+
+                        continue;
+                    }
+                    else if (frameContents[2] == -1)
+                    {
+                        //Insert into the open frame
+                        frames[0][i] = frames[0][i - 1];
+                        frames[1][i] = frames[1][i - 1];
+                        frames[2][i] = ReferenceString[i].ToString() + "(1)";
+
+                        //Push down the queue
+                        frameContents[2] = ReferenceString[i];
+                        continue;
+                    }
+
+                    //Geting to this place means there is a value in every frame spot
+
+                    if (referenceCounts[frameContents[2]] <= referenceCounts[frameContents[0]] &&
+                       referenceCounts[frameContents[2]] <= referenceCounts[frameContents[1]])
+                    {
+                        //If the bottom item is the least value, kick it out
+
+                        frameContents[2] = ReferenceString[i];
+                    }
+                    else if (referenceCounts[frameContents[1]] <= referenceCounts[frameContents[2]] &&
+                             referenceCounts[frameContents[1]] <= referenceCounts[frameContents[0]])
+                    {
+                        frameContents[1] = ReferenceString[i];
+                    }
+                    else
+                    {
+                        frameContents[0] = ReferenceString[i];
+                    }
+
+                    Reorder(frameContents, referenceCounts, ReferenceString[i]);
+
+                    frames[0][i] = frameContents[0] + "(" + referenceCounts[frameContents[0]].ToString() + ")";
+                    frames[1][i] = frameContents[1] + "(" + referenceCounts[frameContents[1]].ToString() + ")";
+                    frames[2][i] = frameContents[2] + "(" + referenceCounts[frameContents[2]].ToString() + ")";
+                }
+            }
+
+            //return faults;  //Number of faults
+            return frames;  //4 rows, top 3 contains numbers, last row contains F's
+        }
+
+        private void Reorder(List<int> frameContents, List<int> referenceCounts, int lastReferenced)
+        {
+            List<int> newOrder = new List<int>(3);
+            newOrder.Add(0);
+            newOrder.Add(0);
+            newOrder.Add(0);
+
+            if (referenceCounts[frameContents[0]] >= referenceCounts[frameContents[1]] &&
+                referenceCounts[frameContents[0]] >= referenceCounts[frameContents[2]])
+            {
+                //The top item is the largest value
+
+                newOrder[0] = frameContents[0];
+                if (referenceCounts[frameContents[1]] >= referenceCounts[frameContents[2]])
+                {
+                    newOrder[1] = frameContents[1];
+                    newOrder[2] = frameContents[2];
+                }
+                else
+                {
+                    newOrder[1] = frameContents[2];
+                    newOrder[2] = frameContents[1];
+                }
+            }
+            else if (referenceCounts[frameContents[1]] >= referenceCounts[frameContents[0]] &&
+                referenceCounts[frameContents[1]] >= referenceCounts[frameContents[2]])
+            {
+                newOrder[0] = frameContents[1];
+                if (referenceCounts[frameContents[0]] >= referenceCounts[frameContents[2]])
+                {
+                    newOrder[1] = frameContents[0];
+                    newOrder[2] = frameContents[2];
+                }
+                else
+                {
+                    newOrder[1] = frameContents[2];
+                    newOrder[2] = frameContents[0];
+                }
+            }
+            else if (referenceCounts[frameContents[2]] >= referenceCounts[frameContents[1]] &&
+                referenceCounts[frameContents[2]] >= referenceCounts[frameContents[0]])
+            {
+                newOrder[0] = frameContents[2];
+                if (referenceCounts[frameContents[0]] >= referenceCounts[frameContents[1]])
+                {
+                    newOrder[1] = frameContents[0];
+                    newOrder[2] = frameContents[1];
+                }
+                else
+                {
+                    newOrder[1] = frameContents[1];
+                    newOrder[2] = frameContents[0];
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                frameContents[i] = newOrder[i];
+            }
+        }
+
         public ObservableCollection<int> ReferenceString { get; set; }
         public int Length { get; set; }
         public int MaxPageValue { get; set; }
